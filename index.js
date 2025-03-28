4008155419651363042ba5923919d2dd629d01d3
Egor763 <132680057+Egor763@users.noreply.github.com>
2025-03-28 12:34:10 +0400

    верстка full

--- index.js
+++
@@ -1,8 +0,0 @@
-const relaxingButton = document.querySelector('.relaxing__cell-button');
-const relaxingText = document.querySelector('.relaxing__cell-text');
-
-function expandText() {
-  relaxingText.classList.add('relaxing__cell-text_active');
-}
-
-relaxingButton.addEventListener('click', expandText);
